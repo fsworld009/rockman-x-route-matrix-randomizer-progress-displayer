ce4519ccf0b5d99bfb34967a270b4ac500acc2e0
WorldFS <fsworld009@gmail.com>
2026-01-22 21:19:19 +0800

    change to white text with black border

--- src/component/ItemIconTextLine.tsx
+++ src/component/ItemIconTextLine.tsx
@@ -87,7 +87,7 @@ function ItemIconTextLine(props: { lines: any[] }) {
 
               <div
                 style="font-family: 'FusionPixelFont12pxMono';"
-                class="flex flex-row items-center gap-2 transition-all duration-400 text-[8vw] tracking-wider text-black [text-shadow:rgb(255,_255,_255)_2px_0px_0px,_rgb(255,_255,_255)_1.75517px_0.958851px_0px,_rgb(255,_255,_255)_1.0806px_1.68294px_0px,_rgb(255,_255,_255)_0.141474px_1.99499px_0px,_rgb(255,_255,_255)_-0.832294px_1.81859px_0px,_rgb(255,_255,_255)_-1.60229px_1.19694px_0px,_rgb(255,_255,_255)_-1.97999px_0.28224px_0px,_rgb(255,_255,_255)_-1.87291px_-0.701566px_0px,_rgb(255,_255,_255)_-1.30729px_-1.51361px_0px,_rgb(255,_255,_255)_-0.421592px_-1.95506px_0px,_rgb(255,_255,_255)_0.567324px_-1.91785px_0px,_rgb(255,_255,_255)_1.41734px_-1.41108px_0px,_rgb(255,_255,_255)_1.92034px_-0.558831px_0px]"
+                class="flex flex-row items-center gap-2 transition-all duration-400 text-[8vw] tracking-wider text-white [text-shadow:rgb(0,0,0)_2px_0px_0px,_rgb(0,0,0)_1.75517px_0.958851px_0px,_rgb(0,0,0)_1.0806px_1.68294px_0px,_rgb(0,0,0)_0.141474px_1.99499px_0px,_rgb(0,0,0)_-0.832294px_1.81859px_0px,_rgb(0,0,0)_-1.60229px_1.19694px_0px,_rgb(0,0,0)_-1.97999px_0.28224px_0px,_rgb(0,0,0)_-1.87291px_-0.701566px_0px,_rgb(0,0,0)_-1.30729px_-1.51361px_0px,_rgb(0,0,0)_-0.421592px_-1.95506px_0px,_rgb(0,0,0)_0.567324px_-1.91785px_0px,_rgb(0,0,0)_1.41734px_-1.41108px_0px,_rgb(0,0,0)_1.92034px_-0.558831px_0px]"
               >
                 <span class="shrink-0">
                   {line[1].split(']')[0]}]
@@ -108,7 +108,7 @@ function ItemIconTextLine(props: { lines: any[] }) {
           {(_, index) => (
             <div
               style="font-family: 'FusionPixelFont12pxMono';"
-              class="flex flex-row items-center gap-2 transition-all duration-400 text-[8vw] tracking-wider text-black [text-shadow:rgb(255,_255,_255)_2px_0px_0px,_rgb(255,_255,_255)_1.75517px_0.958851px_0px,_rgb(255,_255,_255)_1.0806px_1.68294px_0px,_rgb(255,_255,_255)_0.141474px_1.99499px_0px,_rgb(255,_255,_255)_-0.832294px_1.81859px_0px,_rgb(255,_255,_255)_-1.60229px_1.19694px_0px,_rgb(255,_255,_255)_-1.97999px_0.28224px_0px,_rgb(255,_255,_255)_-1.87291px_-0.701566px_0px,_rgb(255,_255,_255)_-1.30729px_-1.51361px_0px,_rgb(255,_255,_255)_-0.421592px_-1.95506px_0px,_rgb(255,_255,_255)_0.567324px_-1.91785px_0px,_rgb(255,_255,_255)_1.41734px_-1.41108px_0px,_rgb(255,_255,_255)_1.92034px_-0.558831px_0px]"
+              class="flex flex-row items-center gap-2 transition-all duration-400 text-[8vw] tracking-wider text-white [text-shadow:rgb(0,0,0)_2px_0px_0px,_rgb(0,0,0)_1.75517px_0.958851px_0px,_rgb(0,0,0)_1.0806px_1.68294px_0px,_rgb(0,0,0)_0.141474px_1.99499px_0px,_rgb(0,0,0)_-0.832294px_1.81859px_0px,_rgb(0,0,0)_-1.60229px_1.19694px_0px,_rgb(0,0,0)_-1.97999px_0.28224px_0px,_rgb(0,0,0)_-1.87291px_-0.701566px_0px,_rgb(0,0,0)_-1.30729px_-1.51361px_0px,_rgb(0,0,0)_-0.421592px_-1.95506px_0px,_rgb(0,0,0)_0.567324px_-1.91785px_0px,_rgb(0,0,0)_1.41734px_-1.41108px_0px,_rgb(0,0,0)_1.92034px_-0.558831px_0px]"
               >
               <span class="shrink-0">[&gt;]</span>
               { displayLines().length === 0 && index() === 0 && (<>
--- src/component/TextAnimated.tsx
+++ src/component/TextAnimated.tsx
@@ -28,7 +28,7 @@ function TextAnimated(props: { text: string }) {
               animate={{ y: '0%', opacity: 1 }}
               exit={{ y: '100%', opacity: 0 }}
               transition={{ duration: 0.3, easing: 'ease' }}
-              class="text-[8vw] text-center text-black [text-shadow:rgb(255,_255,_255)_2px_0px_0px,_rgb(255,_255,_255)_1.75517px_0.958851px_0px,_rgb(255,_255,_255)_1.0806px_1.68294px_0px,_rgb(255,_255,_255)_0.141474px_1.99499px_0px,_rgb(255,_255,_255)_-0.832294px_1.81859px_0px,_rgb(255,_255,_255)_-1.60229px_1.19694px_0px,_rgb(255,_255,_255)_-1.97999px_0.28224px_0px,_rgb(255,_255,_255)_-1.87291px_-0.701566px_0px,_rgb(255,_255,_255)_-1.30729px_-1.51361px_0px,_rgb(255,_255,_255)_-0.421592px_-1.95506px_0px,_rgb(255,_255,_255)_0.567324px_-1.91785px_0px,_rgb(255,_255,_255)_1.41734px_-1.41108px_0px,_rgb(255,_255,_255)_1.92034px_-0.558831px_0px]"
+              class="text-[8vw] text-center text-white [text-shadow:rgb(0,0,0)_2px_0px_0px,_rgb(0,0,0)_1.75517px_0.958851px_0px,_rgb(0,0,0)_1.0806px_1.68294px_0px,_rgb(0,0,0)_0.141474px_1.99499px_0px,_rgb(0,0,0)_-0.832294px_1.81859px_0px,_rgb(0,0,0)_-1.60229px_1.19694px_0px,_rgb(0,0,0)_-1.97999px_0.28224px_0px,_rgb(0,0,0)_-1.87291px_-0.701566px_0px,_rgb(0,0,0)_-1.30729px_-1.51361px_0px,_rgb(0,0,0)_-0.421592px_-1.95506px_0px,_rgb(0,0,0)_0.567324px_-1.91785px_0px,_rgb(0,0,0)_1.41734px_-1.41108px_0px,_rgb(0,0,0)_1.92034px_-0.558831px_0px]"
             >
               <Choootype>
                 {item.key}
